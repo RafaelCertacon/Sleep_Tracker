1a1a1dae157994290bc98c1a68021c07d24cc954
rafael.rocha <rafael.rocha@certacon.com.br>
2024-12-27 15:53:15 -0300

    Feat: Change the Flask path for clean code

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from infra.db_connection import session, SleepTracker
+
+app = Flask("__name__")
+@app.route('/dormir', methods=['POST'])
+def post_dormir():
+    dados = request.get_json()
+    # O operador ** em SleepTracker(**dados) é chamado de desempacotamento de dicionário.
+    # Ele pega um dicionário e transforma suas chaves em argumentos nomeados para o construtor da classe.
+    sleep = SleepTracker(**dados)
+    session.add(sleep)
+    session.commit()
+    return sleep.serialize()
+
+@app.route('/resultados', methods=['GET'])
+def get_sleep():
+    all_resultados = session.query(SleepTracker).all()
+    return [resultado.serialize()
+            for resultado in all_resultados]
+
+@app.route('/apagar_tempo/<int:id>', methods=['DELETE'])
+def delete_tempo(id):
+    delete_linha = session.query(SleepTracker).get(id)
+    session.delete(delete_linha)
+    session.commit()
+    return jsonify({
+        'mensagem': 'Registro deletado com sucesso',
+        'id_deletado': id
+    }), 200
--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-from flask import Flask, make_response, jsonify, request
-from infra.db_connection import db, session, SleepTracker
+from app import app
 
-app = Flask("__name__")
+port = 2020
 
-@app.route('/dormir', methods=['POST'])
-def post_dormir():
-    dados = request.get_json()
-    # O operador ** em SleepTracker(**dados) é chamado de desempacotamento de dicionário.
-    # Ele pega um dicionário e transforma suas chaves em argumentos nomeados para o construtor da classe.
-    sleep = SleepTracker(**dados)
-    session.add(sleep)
-    session.commit()
-    return sleep.serialize()
-
-@app.route('/resultados', methods=['GET'])
-def get_sleep():
-    all_resultados = session.query(SleepTracker).all()
-    return [resultado.serialize()
-            for resultado in all_resultados]
-
-@app.route('/apagar_tempo/<int:id>', methods=['DELETE'])
-def delete_tempo(id):
-    delete_linha = session.query(SleepTracker).get(id)
-    session.delete(delete_linha)
-    session.commit()
-    return jsonify({
-        'mensagem': 'Registro deletado com sucesso',
-        'id_deletado': id
-    }), 200
-
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True, port=port, host='0.0.0.0')
