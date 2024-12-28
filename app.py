7ca4602f06837f83ddac3382d6be6abc65f84efa
rafael.rocha <rafael.rocha@certacon.com.br>
2024-12-28 14:45:59 -0300

    Feat: Add function login with method POST

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, jsonify, request
 from infra.db_connection import session, SleepTracker
 
 app = Flask("__name__")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if username == "admin" and password == "12345":
+        return jsonify({"message": "Acesso com sucesso!"}),200
+    if username != "admin" and password != "<PASSWORD>":
+        return jsonify({"message": "Erro ao digitar username ou senha!"}),401
+    else:
+        return jsonify({"message": "Erro ao fazer o login"}),401
 @app.route('/dormir', methods=['POST'])
 def post_dormir():
     dados = request.get_json()
