718fbd20e2cff9788ff87918f8128ece223b827c
rafael.rocha <rafael.rocha@certacon.com.br>
2025-01-03 16:35:23 -0300

    Feat: Correct token JWT

--- authentication/authenticate.py
+++ authentication/authenticate.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from app import *
+from flask import request, current_app
+import jwt
 
 def token_required(f):
     @wraps(f)
@@ -8,8 +9,12 @@ def token_required(f):
         if not token:
             return {'message': 'Token ausente!'}, 401
         try:
-            jwt.decode(token, app.config['SECRET_KEY'], algorithms=["RS256"])
-        except:
+            token = token.split(" ")[1]
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return {'message': 'Token expirado!'}, 401
+        except jwt.InvalidTokenError:
             return {'message': 'Token inválido!'}, 401
         return f(*args, **kwargs)
+
     return decorated
